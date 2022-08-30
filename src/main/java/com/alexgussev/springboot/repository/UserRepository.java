823ef05646c10023c8ef9c8dc9d8e5fe5c408e98
AlexKing <pikashev171@mail.ru>
2022-08-30 06:44:13 +0500

    my commit

--- src/main/java/com/alexgussev/springboot/repository/UserRepository.java
+++ src/main/java/com/alexgussev/springboot/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.alexgussev.springboot.repository;
 
 import com.alexgussev.springboot.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
